08ac4ccfc2a7a7642b3ee156c11b2e7092431a7b
thusitha <thusitha.dissanayaka@rezgateway.com>
2020-05-05 12:37:12 +0530

    added

--- src/main/java/com/example/springbootwebapplication/controller/CustomerDataController.java
+++ src/main/java/com/example/springbootwebapplication/controller/CustomerDataController.java
@@ -96,37 +96,4 @@ public class CustomerDataController {
 	public String servicecheck() {
 		return "This is the First Message From Remote common-data-service!";
 	}
-
-	@RequestMapping(value = "/testData", method = RequestMethod.GET, produces = "application/json")
-	public ResponseEntity<String> testData() {
-		String results = "";
-		try {
-			JSONArray array = new JSONArray();
-
-			JSONObject json = new JSONObject();
-			json.accumulate("cityname", "London");
-			json.accumulate("statecode", "");
-			json.accumulate("countrycode", "GB");
-			json.accumulate("vendorcitycode", "20142");
-			json.accumulate("countryname", "United Kingdom");
-
-			array.add(json);
-
-			json = new JSONObject();
-			json.accumulate("cityname", "Lancaster");
-			json.accumulate("statecode", "");
-			json.accumulate("countrycode", "GB");
-			json.accumulate("vendorcitycode", "23711");
-			json.accumulate("countryname", "United Kingdom");
-
-			array.add(json);
-
-			results = array.toString();
-		} catch (Exception e) {
-			e.printStackTrace();
-		}
-
-		return ResponseEntity.ok(results);
-	}
-
 }
