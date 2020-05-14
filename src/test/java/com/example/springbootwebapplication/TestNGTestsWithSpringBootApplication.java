e5cc550a4df6c0216de4b19bc500d144729be1ee
thusitha <thusitha.dissanayaka@rezgateway.com>
2020-05-14 11:37:54 +0530

    added

--- src/test/java/com/example/springbootwebapplication/TestNGTestsWithSpringBootApplication.java
+++ src/test/java/com/example/springbootwebapplication/TestNGTestsWithSpringBootApplication.java
@@ -3,6 +3,7 @@ package com.example.springbootwebapplication;
 import org.testng.Assert;
 import org.testng.annotations.AfterClass;
 import org.testng.annotations.BeforeClass;
+import org.testng.annotations.DataProvider;
 import org.testng.annotations.Test;
 
 public class TestNGTestsWithSpringBootApplication extends BaseIT {
@@ -22,8 +23,18 @@ public class TestNGTestsWithSpringBootApplication extends BaseIT {
 		Assert.assertEquals(2 * 2, 4, "2x2 should be 4");
 	}
 
+	@Test(dataProvider = "numberGenerator")
+	public void testParameter(int num1, int num2, int output) {
+		Assert.assertEquals(num1 * num2, output, "Out put should be:" + output);
+	}
+
 	@AfterClass
 	public void tearDown() {
 		System.out.println("I am in tearDown");
+	}
+
+	@DataProvider(name = "numberGenerator")
+	public Object[][] getValues() {
+		return new Object[][] { { 2, 2, 4 }, { 5, 10, 50 }, { 7, 8, 56 } };
 	}
 }
